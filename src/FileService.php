505a1240f93dbcc2753bd8927d9d23da4b235af3
Jose Bernal <jbernal.web.dev@gmail.com>
2022-06-24 20:31:14 -0700

    External class for additional file-related api calls.

---
+++ src/FileService.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+class FileService {
+
+
+	public function downloadContentDocument($id){
+
+		$api = $this->loadForceApi();
+
+		$query = "SELECT VersionData, Title FROM ContentVersion WHERE ContentDocumentId = '$id' AND IsLatest = True";
+
+		$version = $api->query($query)->getRecord();
+		$versionUrl = $version["VersionData"];
+
+		$api2 = $this->loadForceApi();
+		$resp = $api2->send($versionUrl);
+
+		$file = new File($version["Title"]);
+		$file->setContent($resp->getBody());
+		$file->setType($resp->getHeader("Content-Type"));
+
+		return $file;
+	}
+
+
+
+}
