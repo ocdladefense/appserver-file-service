d6b70984afeecf5887e26fdb1db03874e256130a
Jose Bernal <jbernal.web.dev@gmail.com>
2022-06-24 20:30:46 -0700

    Rename template file.

---
+++ templates/list.tpl.php
@@ -0,0 +1,27 @@
+<h1 class="list-header">Available Documents</h1>
+
+
+
+<div class="table">
+    <div class="table-row first">
+        <p class="table-header">Title</p>
+        <p class="table-header">Type</p>
+        <p class="table-header">Size</p>
+    </div>
+
+
+
+
+    <?php foreach($links as $link): ?>
+
+        <div class="table-row data">
+            <p class="table-cell"><?php print $link["ContentDocument"]["Title"]; ?></p>
+            <p class="table-cell"><?php print $link["ContentDocument"]["FileExtension"]; ?></p>
+            <p class="table-cell"><?php print $link["ContentDocument"]["ContentSize"] . " kb"; ?></p>
+            <p class="table-cell"><a href="/file/download/<?php print $link["ContentDocumentId"]; ?>"><i class="fa-solid fa-download"></i></a></p>
+        </div>
+    <?php endforeach; ?>
+
+</div>
+
+
