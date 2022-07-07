b077ac6eeeb4b326a2f7a2734384a9672f66eb5e
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-07-07 12:53:26 -0700

    Show the sharing options in a linear format.

--- components/upload/templates/form.tpl.php
+++ components/upload/templates/form.tpl.php
@@ -1,6 +1,18 @@
 
 <form method="post" action="/file/upload/file" enctype="multipart/form-data">
 
+	<div class="form-item">
+		<input type="file" id="Attachments__c[]" name="Attachments__c[]"  required />
+	</div>
+
+	<div class="form-item">
+		<input type="submit" value="Upload File" />
+	</div>
+
+	<br />
+	<br />
+
+	
 	<div class="sharing">
 		<h3>Sharing With:</h3>
 		<?php foreach($sharing as $objectId => $label): ?>
@@ -11,14 +23,6 @@
 		<?php endforeach; ?>
 
 		<input type="hidden" name="linkedEntityIds[]" value="<?php print $contactId; ?>">
-	</div>
-
-	<div class="form-item">
-		<input type="file" id="Attachments__c[]" name="Attachments__c[]"  required />
-	</div>
-
-	<div class="form-item">
-		<input type="submit" value="Upload File" />
 	</div>
 
 </form>
